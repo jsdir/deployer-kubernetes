a8d254af0c527ee69137fd6570663eee2d8b19d7
Jason Sommer <jsdirv@gmail.com>
2015-02-10 17:03:33 -0600

    Fix container caching

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -3,9 +3,9 @@ set -e
 
 # Start a kubernetes cluster.
 # llamashoes/dind-kubernetes
-docker run -d -p 127.0.0.1:8888:8888 --privileged bzsarwilgcx774cgqbutbvk
+docker run -d -p 127.0.0.1:8888:8888 --privileged llamashoes/dind-kubernetes
 
 # Block until the service starts.
 echo "Blocking until server starts..."
-wget --tries=30 --retry-connrefused --waitretry=1 http://localhost:8888 &> /dev/null
+wget --tries=30 --retry-connrefused --waitretry=1 --spider http://localhost:8888
 echo "Server at localhost:8888 started."
