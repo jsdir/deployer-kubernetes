1d0da151b55330b08c61f63c8d65f67c1c8cf8d9
Jason Sommer <jsdirv@gmail.com>
2015-02-10 06:10:59 -0600

    Block until the service starts

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -2,4 +2,7 @@
 set -e
 
 # Start a kubernetes cluster.
-docker run --privileged --net="host" llamashoes/dind-kubernetes
+docker run -d --privileged --net="host" llamashoes/dind-kubernetes
+
+# Block until the service starts.
+wget --tries=30 --retry-connrefused --waitretry=1 http://localhost:8888 &> /dev/null
