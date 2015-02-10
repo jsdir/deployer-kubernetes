ff70028d8c270e9e15445098d72015f7734b0ecb
Jason Sommer <jsdirv@gmail.com>
2015-02-10 16:47:26 -0600

    Use build id for booting cluster

--- scripts/start-cluster.sh
+++ scripts/start-cluster.sh
@@ -2,7 +2,10 @@
 set -e
 
 # Start a kubernetes cluster.
-docker run -d -p 8888:8888 --privileged --net="host" llamashoes/dind-kubernetes
+# llamashoes/dind-kubernetes
+docker run -d -p 127.0.0.1:8888:8888 --privileged bzsarwilgcx774cgqbutbvk
 
 # Block until the service starts.
+echo "Blocking until server starts..."
 wget --tries=30 --retry-connrefused --waitretry=1 http://localhost:8888 &> /dev/null
+echo "Server at localhost:8888 started."
